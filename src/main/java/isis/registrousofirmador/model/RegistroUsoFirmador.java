c96f07145602e6887a0862e53233307eec96679a
mbrachetta <mbrachetta@gmail.com>
2015-02-09 21:21:35 -0300

    algo

--- src/main/java/isis/registrousofirmador/model/RegistroUsoFirmador.java
+++ src/main/java/isis/registrousofirmador/model/RegistroUsoFirmador.java
@@ -7,12 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @SuppressWarnings("serial")
 @Entity
